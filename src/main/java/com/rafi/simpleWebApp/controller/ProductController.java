d16cfca374c6b7c828638cc5dfbf28a0e5eb9b6e
SadikHasanRafi <sadikkhasan@gmail.com>
2025-03-24 13:07:20 +0600

    create new method compelte

--- src/main/java/com/rafi/simpleWebApp/controller/ProductController.java
+++ src/main/java/com/rafi/simpleWebApp/controller/ProductController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafi.simpleWebApp.model.Product;
 import com.rafi.simpleWebApp.service.ProductServoce;
+
+
 
 @RestController
 public class ProductController {
@@ -28,4 +32,11 @@ public class ProductController {
         // Implement logic to get product by id
         return service.getProductById(prodId);
     }
+
+
+    @PostMapping("/products")
+    public Product requestMethodName(@RequestBody Product param) {
+        return service.addProduct(param);
+    }
+    
 }
--- src/main/java/com/rafi/simpleWebApp/service/ProductServoce.java
+++ src/main/java/com/rafi/simpleWebApp/service/ProductServoce.java
@@ -1,8 +1,8 @@
 package com.rafi.simpleWebApp.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -11,25 +11,29 @@ import com.rafi.simpleWebApp.model.Product;
 @Service
 public class ProductServoce {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
         new Product(110, "iPhone 15", 1200),
         new Product(111, "iPhone 16", 1300),
         new Product(112, "Samsung Galaxy S24", 1100),
         new Product(113, "Google Pixel 8", 1000),
         new Product(114, "OnePlus 12", 900),
         new Product(115, "Xiaomi 14 Pro", 850)
-    );
+    ));
 
     public List<Product> getAllProducts(){
-
 
         return products;
         // return "All products";
     }
 
-
     public Product getProductById(int id){
         // return "Product with id: " + id;
         return products.stream().filter(product -> product.id == id).findFirst().orElse(new Product(100, "No such Mobile", 0));
+    }
+
+    public Product addProduct(Product product) {
+        products.add(product);
+        
+        return getProductById(product.id);
     }
 }
