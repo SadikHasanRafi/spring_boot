c2778838c7501fad2869acc91e3f9cae155bd33e
SadikHasanRafi <sadikkhasan@gmail.com>
2025-04-17 12:36:59 +0600

    main branch created

--- src/main/java/com/myApp/MyApp/MyAppApplication.java
+++ src/main/java/com/myApp/MyApp/MyAppApplication.java
@@ -1,10 +1,8 @@
 package com.myApp.MyApp;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+
+
 public class MyAppApplication {
 
 	public static void main(String[] args) {
